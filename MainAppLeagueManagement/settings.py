0a8603db2982ba9f02153c7731c4e903bed90218
MuxyNigel <150058505+MuxyNigel@users.noreply.github.com>
2026-02-06 00:29:42 +0200

    HOME IMAGE SETTINGS

--- MainAppLeagueManagement/settings.py
+++ MainAppLeagueManagement/settings.py
@@ -145,3 +145,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# No STATICFILES_DIRS needed if using app-level static folders
+STATIC_ROOT = BASE_DIR / "staticfiles"
